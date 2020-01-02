3bad476d8c7fd65afdc7c41e97fd49c2aafaf25b
anhquangtong2000@gmail.com <quanganh*2000>
2020-01-02 12:51:38 +0700

    add file apptemplate new

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include_once "partials/head.php";?>
+</head>
+<body>
+
+<!-- Header -->
+    <?php include_once "partials/header.php";?>
+<!-- /Header -->
+
+
+<!-- Section -->
+    <?php
+	if(isset($_GET['page']) && $_GET['page']) {
+		$filePath = dirname(__FILE__) . "/pages/". trim($_GET['page']) . ".php";
+		echo "<br> FILE PATH : " . $filePath;
+
+		include_once "pages/". trim($_GET['page']) . ".php";
+	}
+	else{
+        include_once "pages/home.php";
+	}
+
+
+	?>
+<!-- Section -->
+
+<!-- Footer -->
+    <?php include_once "partials/footer.php";?>
+<!-- /Footer -->
+
+<!-- jQuery Plugins -->
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/js/bootstrap.min.js"></script>
+<script src="assets/js/main.js"></script>
+
+</body>
+</html>
